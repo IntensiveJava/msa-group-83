740cc56da6aa084660c1b08231e8ba3e1013bef5
Sergey Ivanov <SG.Ivanov@dev.vtb.ru>
2024-10-28 19:44:31 +0300

    add tour-server

---
+++ tour-service/src/main/java/com/aston/intensive/tourservice/controller/NewController.java
@@ -0,0 +1,15 @@
+package com.aston.intensive.tourservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class NewController {
+
+    @GetMapping("/name")
+    public String name() {
+        return "New Controller";
+    }
+}
